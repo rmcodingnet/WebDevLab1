d31813385f1541de112ac03774fc2b1a5dc50b50
1507079 <rory912@btinternet.com>
2016-10-03 15:52:11 +0100

    Added New Page "SMSR3.php" with array echo.

---
+++ SMSR3.php
@@ -0,0 +1,23 @@
+<?php
+$provisionedActivities = array("Specs","Drugs","Rock and Roll")
+
+
+
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Activity - Specs, Mugs and Sausage Rolls III</title>
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+<body>
+<p><?php foreach($provisionedActivity as $x) {
+        print "<p>$x</p>";
+    }?></p>
+</body>
+</html>
