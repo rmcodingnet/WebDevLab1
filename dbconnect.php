c5a4084a782bf6cb70fb5d681d7faa21910a4e6d
1507079 <rory912@btinternet.com>
2016-10-10 16:01:31 +0100

    reworked file again using different mysqli connector

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,11 @@
 <?php
 // connect to server and select database
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'b69cae8485cbb8');
+define('DB_PASSWORD', 'b871ddfb');
+define('DB_DATABASE', '1507079db');
 
-$db = new mysqli('eu-cdbr-azure-north-e.cloudapp.net',
-    'b69cae8485cbb8',
-    'b871ddfb',
-    '1507079db');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 // test if connection was established and print ant errors
 if($db->connect_errno){
