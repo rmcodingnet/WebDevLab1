94736d3120318b6d08069963f911b4bd558b1301
1507079 <rory912@btinternet.com>
2016-10-24 15:18:40 +0100

    commit cookie

--- Week5/homepage.php
+++ Week5/homepage.php
@@ -11,9 +11,9 @@ $accesslevel = $_COOKIE['access_level'];
 </head>
 <body>
 <?php
-if($accesslevel = "standarduser") {
+if($accesslevel == "standarduser") {
     echo '<p>OH LOOK A STANDARD USER</p>';
-} elseif($accesslevel = "root") {
+} elseif($accesslevel === "root") {
    echo "<p>OH LOOK THE ROOT NOW YOU HAVE ACCESS TO EVERYTHING ISN'T THAT FUN?!?!?!?!?!?!</p>";
 } else {
     echo "<p>HOW THE HELL DID YOU EVEN GET HERE?</p>";
