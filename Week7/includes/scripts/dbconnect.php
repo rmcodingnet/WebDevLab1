056a068a6ab6545d1a05bff0a274679a342bde36
1507079 <rory912@btinternet.com>
2016-11-07 16:07:32 +0000

    commit

--- Week7/includes/scripts/dbconnect.php
+++ Week7/includes/scripts/dbconnect.php
@@ -5,7 +5,6 @@ $db = new mysqli(
     "b69cae8485cbb8",
     "b871ddfb",
     "1507079db" );
-
 // test our connection
 if ($db->connect_errno) {
     die ('Connection Failed :'.$db->connect_error );
