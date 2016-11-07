3b75be9d8c2019efa74a0021048bfd3ef280e641
1507079 <rory912@btinternet.com>
2016-11-07 14:30:47 +0000

    commit week 7 db initialisation

---
+++ Week 7/includes/scripts/dbconnect.php
@@ -0,0 +1,13 @@
+<?PHP
+
+// connect to your Azure server and select database (remember you connection details are all on the azure portal
+$db = new mysqli(
+    "eu-cdbr-azure-north-e.cloudapp.net",
+    "b69cae8485cbb8",
+    "b871ddfb",
+    "1507079db" );
+
+// test our connection
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
+}
